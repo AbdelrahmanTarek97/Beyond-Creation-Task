1157d1cf03e61832b9fdbee0a4903e3ab2908a53
AbdelrahmanTarek97 <Abdelrahman_Tarek251@hotmail.com>
2022-03-31 15:11:21 +0200

    Push strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '777cf36e700285cbaf0f72662f15b75c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9bf6b668-e21a-44d7-8f3a-7d8f063600af'
+};
